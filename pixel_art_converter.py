3ba76ca67b3af80102187d484eabbecbdf8912d2
maixhashi <maixhashi@gmail.com>
2024-12-27 22:29:12 +0900

    Build_2024.12.27.22.28_ピクセルアートファイルの出力ファイルのサイズを固定化

--- pixel_art_converter.py
+++ pixel_art_converter.py
@@ -11,9 +11,4 @@ def image_to_pixel_art(image_path, output_path, target_size=(32, 32)):
     img_pixel_art.save(output_path)
 
 # 使用例
-image_to_pixel_art('input_image.jpg', 'output_pixel_art_16x16.png', target_size=(16, 16))
-image_to_pixel_art('input_image.jpg', 'output_pixel_art_20x20.png', target_size=(20, 20))
-image_to_pixel_art('input_image.jpg', 'output_pixel_art_24x24.png', target_size=(24, 24))
-image_to_pixel_art('input_image.jpg', 'output_pixel_art_32x32.png', target_size=(32, 32))
-image_to_pixel_art('input_image.jpg', 'output_pixel_art_64x64.png', target_size=(64, 64))
 image_to_pixel_art('input_image.jpg', 'output_pixel_art_128x128.png', target_size=(128, 128))
