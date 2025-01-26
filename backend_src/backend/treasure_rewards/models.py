9e2ecf89027efd0c2d5861b1eee23258a81adb96
maixhashi <maixhashi@gmail.com>
2025-01-26 09:53:34 +0900

    Feature_2025.01.26.09.53_招集の映画ポスターの補完機能を実装
    [変更内容]
     - バックエンド / Django
       - データベースに保存されているtmdbのidに対応するポスター画像ファイルがassetsファイルに保存されていないとき、保管する機能を実装

--- backend_src/backend/treasure_rewards/models.py
+++ backend_src/backend/treasure_rewards/models.py
@@ -1,21 +1,17 @@
-import os
-import re
 from django.db import models
-from django.utils.text import slugify
+import os
 
-from django.db import models
-import os
 
 def upload_to(instance, filename):
-    # ファイル名の生成 (例: pixelized_<tmdb_id>.png)
+    # ファイル名を生成 (例: pixelized_<tmdb_id>.png)
     return f"pixel_art/{filename}"
 
-    pixel_art_image = models.ImageField(upload_to=upload_to)
 
 class RewardImage(models.Model):
+    tmdb_id = models.CharField(max_length=20, unique=True)  # TMDB IDを一意に管理
     title = models.CharField(max_length=255)  # 映画タイトル
     original_poster_url = models.URLField()  # 元のポスターURL
-    pixel_art_image = models.ImageField(upload_to=upload_to, null=True, blank=True)
+    pixel_art_image = models.ImageField(upload_to=upload_to, null=True, blank=True)  # ピクセルアート画像
     created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
 
     def __str__(self):
