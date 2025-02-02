6daa0d057315e3be979b4ef5fb567aface491202
maixhashi <maixhashi@gmail.com>
2025-02-03 04:08:03 +0900

    Build_2025.02.03.04.11_Herokuのデプロイ設定

--- backend_src/backend/backend/settings.py
+++ backend_src/backend/backend/settings.py
@@ -20,7 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)g^vl5izw+^illctzd(log$2&=b7syl1w@6^&%ve(x9b*))2%l'
+from dotenv import load_dotenv
+import os
+
+# .envファイルを読み込む
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -166,3 +173,19 @@ CSRF_COOKIE_HTTPONLY = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",  # ReactのURL
 ]
+
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Heroku の環境変数を考慮
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
