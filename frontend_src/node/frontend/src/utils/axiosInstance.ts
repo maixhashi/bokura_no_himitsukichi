081f1ba19e7a6bfec0ea99f6fbf9e8a0f307cbe1
maixhashi <maixhashi@gmail.com>
2025-02-04 07:32:19 +0900

    Modify_2025.02.04.07.32

--- frontend_src/node/frontend/src/utils/axiosInstance.ts
+++ frontend_src/node/frontend/src/utils/axiosInstance.ts
@@ -30,17 +30,3 @@ const getCsrfToken = async () => {
 getCsrfToken();
 
 export default axiosInstance;
-
-// const myBaseUrl = 'http://127.0.0.1:8000/';
-
-
-const AxiosInstance = axios.create({
-    baseURL: myBaseUrl,
-    timeout: 5000,
-    headers: {
-        "Content-Type":"application/json",
-        accept: "application/json"
-    }
-});
-
-export default AxiosInstance
