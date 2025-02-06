0901d9d6ab24415bdfac64a0549e903b5b6a6429
maixhashi <maixhashi@gmail.com>
2025-02-06 21:53:07 +0900

    Build_2025.02.06.21.53_build.shの処理順序を変更
    [変更内容]
     - データベースmigrateをtmdbの映画情報呼び出しの前に変更

--- backend_src/build.sh
+++ backend_src/build.sh
@@ -4,9 +4,9 @@ cd backend_src
 pip install -r requirements_prod.txt
 
 cd backend
+python manage.py migrate
 python manage.py fetch_and_create_rewards
 python manage.py collectstatic --no-input
-python manage.py migrate
 
 
 # if [[ $CREATE_SUPERUSER ]]
