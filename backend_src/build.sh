37eecc88746c7e08442abe892b521b03bb5b1ea1
maixhashi <maixhashi@gmail.com>
2025-02-08 22:45:03 +0900

    Build_2025.02.08.22.45_デプロイエラーに対する修正

--- backend_src/build.sh
+++ backend_src/build.sh
@@ -1,10 +1,13 @@
 set -o errexit
 
+export DJANGO_ENV=production
+
 # フロントエンドの dist/movie_posters を作成
 mkdir -p /opt/render/project/src/frontend_src/node/frontend/dist/movie_posters
 
 # Django の `collectstatic` 用のディレクトリも作成
 mkdir -p /opt/render/project/src/backend_src/backend/staticfiles
+mkdir -p /opt/render/project/src/backend_src/backend/dist/movie_posters
 
 cd backend_src
 pip install -r requirements_prod.txt
